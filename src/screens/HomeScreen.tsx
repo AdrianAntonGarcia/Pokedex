8d5a970a02a41158470491fa362b537d1108722d
adriton <adrtler@gmail.com>
2021-11-03 19:20:02 +0100

    fadeInImage y useAnimation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,11 +3,12 @@ import {ActivityIndicator, FlatList, Image, Text} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
 import {appStyles} from '../theme/appTheme';
+import {FadeInImage} from '../components/FadeInImage';
 
 export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
   const {simplePokemonList, loadPokemons} = usePokemonPaginated();
-  console.log(simplePokemonList);
+
   return (
     <>
       <Image
@@ -19,9 +20,7 @@ export const HomeScreen = () => {
         keyExtractor={pokemon => pokemon.id}
         showsVerticalScrollIndicator={false}
         renderItem={({index, item}) => (
-          <Image
-            source={{uri: item.picture}}
-            style={{width: 100, height: 100}}></Image>
+          <FadeInImage uri={item.picture} style={{width: 100, height: 100}} />
         )}
         // Infinite scroll
         onEndReached={loadPokemons}
