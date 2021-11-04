6f4ecaec7dbfbba5e5e0a482f8c4fed1c3a3d393
adriton <adrtler@gmail.com>
2021-11-04 16:29:29 +0100

    Diseño inicial para la búsqueda de pokemon

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const SearchInput = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.textBackground}>
+        <TextInput
+          placeholder="Buscar Pokémon"
+          style={styles.textInput}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Icon name="search-outline" color="grey" size={30} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: 'red',
+  },
+  textBackground: {
+    backgroundColor: '#F3F1F3',
+    borderRadius: 50,
+    height: 40,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.39,
+    shadowRadius: 8.3,
+    elevation: 13,
+  },
+  textInput: {flex: 1, fontSize: 18, top: 2},
+});
